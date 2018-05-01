440525a06ca10f366005bb3ef1d75dcc30a3e8ac
Alisson Dias <alissondiascangere@hotmail.com>
2018-05-01 00:02:20 -0300

    [master] Criacao da estrutura

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const logger = require('./helpers/logger')
+
+app.use(function (req, res, next) {
+    logger.info(req.ip);
+    next();
+});
+
+//Rotas
+const index = require('./routers/index');
+const heroRoute = require('./routers/herores.router');
+app.use('/', index);
+app.use('/heroes', heroRoute);
+module.exports = app;
